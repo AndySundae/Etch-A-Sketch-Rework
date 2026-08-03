f021c80f96bbe12da867a645411dea5993b18170
AndySundae <285707921+AndySundae@users.noreply.github.com>
2026-08-03 10:16:17 -0400

    Add extra features "crazy mode"

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let container = document.getElementById("container")
 let sizeBtn = document.getElementById("size")
+let crazyBtn = document.getElementById("crazy")
+
+let mode;
 let pixels;
 
+// create func that creates the squares
 function gridMaker() {
     container.replaceChildren()
     gridSize = window.prompt("Pixels (max 100)")
@@ -12,6 +16,7 @@ function gridMaker() {
     for (let i = 0; i < containerSize; i++) {
         div = document.createElement("div")
         div.classList.add("pixel")
+        if (mode === "crazy") div.style.backgroundColor = `rgba(${randomInt()}, ${randomInt()}, ${randomInt()})`;
         container.appendChild(div)
     }
     getPixels()
@@ -23,14 +28,30 @@ function getPixels() {
     pixels = document.querySelectorAll(".pixel")
 }
 
+sizeBtn.addEventListener("click", () => {
+    mode = "normal"
+    gridMaker()
+    pixelHover()
+})
+
+crazyBtn.addEventListener("click", () => {
+    mode = "crazy"
+    gridMaker()
+    pixelHover()
+})
+
+
 function pixelHover(){
     pixels.forEach(pixel => {
+        pixel.dataset.alpha = 1;
         pixel.addEventListener("mouseenter", () => {
             pixel.classList.add("pixelHover")
+            if (mode === "crazy") pixel.style.opacity = `${pixel.dataset.alpha -= 0.1}`
         })
     });
 }
 
-sizeBtn.addEventListener("click", gridMaker)
-
+function randomInt() {
+    return Math.floor(Math.random() * 256)
+}
 
